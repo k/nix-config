ad416084922e1b7c8640d5c7453194dc1fe01648
thekenner33 <k.bambridge@me.com>
2013-03-22 13:36:28 -0400

    FUCK

---
+++ Developer/Tutorials/Objective-C/myfind/myfind/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  myfind
+//
+//  Created by Kenneth Bambridge on 3/8/13.
+//  Copyright (c) 2013 Kenneth Bambridge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
